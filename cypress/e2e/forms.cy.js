1c114f6c8e282b7b2cf29ef8751b09d16e63b72f
marco <marcogomesr@gmail.com>
2023-08-15 14:35:20 +0200

    fix form test

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -11,17 +11,18 @@ describe('forms test', () => {
     cy.getDataTest('subscribe-button').click()
     cy.contains(/Successfully subbed: marcogomesr@gmail.com!/i).should('exist')
     cy.wait(3000)
-    cy.contains(/Successfully subbed: marcogomesr@gmail.com!/i).should('no.exist')
+    cy.contains(/Successfully subbed: marcogomesr@gmail.com!/i).should('not.exist')
 
     cy.get('@subcribe-input').type('marcogomesr@gmail.io')
-    cy.contains(/invalid email: marcogomesr@gmail.io!/i).should('no.exist')
+    cy.contains(/invalid email: marcogomesr@gmail.io!/i).should('not.exist')
     cy.getDataTest('subscribe-button').click()
     cy.contains(/invalid email: marcogomesr@gmail.io!/i).should('exist')
     cy.wait(3000)
-    cy.contains(/invalid email: marcogomesr@gmail.io!/i).should('no.exist')
+    cy.contains(/invalid email: marcogomesr@gmail.io!/i).should('not.exist')
 
-    cy.contains(/fails!/i).should('no.exist')
+    
+    cy.contains(/fail!/i).should('not.exist')
     cy.getDataTest('subscribe-button').click()
-    cy.contains(/fails!/i).should('exist')
+    cy.contains(/fail!/i).should('exist')
   })
 })
